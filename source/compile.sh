d31b40f1f66508884e21bccc5f028b32f939ce37
André Veríssimo <afsverissimo@gmail.com>
2017-02-26 22:49:39 +0000

    needs to compile in 32bits to work

--- source/compile.sh
+++ source/compile.sh
@@ -2,5 +2,5 @@ chmod 755 *.html
 chmod 755 *.php
 chmod 755 *.h
 chmod 755 *.c
-gcc -g sequencefile.c statistics.c suffixtries.c bitmap.c graphics.c 3dgraphics.c entropicprofiles.c -lm -o ep
+gcc -m32 -g sequencefile.c statistics.c suffixtries.c bitmap.c graphics.c 3dgraphics.c entropicprofiles.c -lm -o ep
 chmod 755 ep
